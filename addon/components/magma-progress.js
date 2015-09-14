4305c6f6ce1e6c26db67d830e4fd70ab2a6d4452
Alexis <Alexis@Alexis-MacBook-Pro.local>
2015-09-14 16:51:40 -0400

    Add progress component

---
+++ addon/components/magma-progress.js
@@ -0,0 +1,42 @@
+/**
+ * Progress
+ *
+ * @class Magma.Component.Progress
+ *
+ */
+
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+	attributeBindings: [
+		'ariaValuemin:aria-valuemin',
+		'ariaValuemax:aria-valuemax',
+		'ariaValuenow:aria-valuenow',
+		'style',
+		'role',
+		'title'
+	],
+
+	ariaValuemin: 0,
+
+	ariaValuemax: Ember.computed.alias('valueMax'),
+
+	ariaValuenow: Ember.computed.alias('value'),
+
+	classNames: ['magma-progress'],
+
+	indicatorValue: Ember.computed('value', 'valueMax', function () {
+		return parseFloat(this.get('value')*100/this.get('valueMax'));
+	}),
+
+	label: void 0,
+
+	role: 'progressbar',
+
+	title: void 0,
+
+	value: 0,
+
+	valueMax: 100
+});
