f7918683dcce5668ce66ee897c2cdbab02472555
Alexis Mangin <me@alexismangin.com>
2015-09-17 00:34:13 -0400

    Refactor selectbox

--- addon/components/magma-selectbox.js
+++ addon/components/magma-selectbox.js
@@ -10,10 +10,12 @@ import DisabledSupport from 'magma/mixins/disabled-support';
 import InvalidSupport from 'magma/mixins/invalid-support';
 import RequiredSupport from 'magma/mixins/required-support';
 
-export default Ember.Select.extend(
+export default Ember.Component.extend(
 	DisabledSupport,
 	InvalidSupport,
 	RequiredSupport, {
+
+	action: void 0,
 
 	attributeBindings: [
 		'aria-describedby',
@@ -21,5 +23,33 @@ export default Ember.Select.extend(
 		'aria-labelledby'
 	],
 
-	classNames: ['magma-selectbox']
+	classNames: ['magma-selectbox'],
+
+	content: null,
+
+	prompt: null,
+
+	optionValuePath: 'id',
+
+	optionLabelPath: 'title',
+
+	selection: void 0,
+
+	tagName: 'select',
+
+	selectboxInit: Ember.on('init', function () {
+		if (!this.get('content')) {
+			this.set('content', []);
+		}
+	}),
+
+	selectionDidChange: Ember.on('change', function () {
+		const selectedIndex = this.$()[0].selectedIndex;
+		const selection = this.get('content')[!!this.get('prompt') ? selectedIndex - 1 : selectedIndex];
+		const action = this.get('action');
+		this.set('selection', selection);
+		if (action) {
+			action(selection);
+		}
+	})
 });
